f76f5d9bc2fbe5dcf38782ae3613aea9844a6231
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2016-10-15 00:50:56 +0200

    Sketching the game UI + event handling.

--- main.lisp
+++ main.lisp
@@ -12,53 +12,119 @@
 
 (defvar *title-text-style* (clim:make-text-style :fix :bold 24))
 
-(defun make-game-cell (x y)
-  (clim:make-pane 'clim:push-button
-                  :label (format nil "(~A ~A)" x y)
-                  :width 64
-                  :height 64))
+(defvar *game-highscore* 0)
 
+
+;;; game UI elements and layout
 (clim:define-application-frame 2048-game
-    () ()
+    ()
+  ((game-board :initform (make-array '(4 4))
+               :accessor game-board)
+   (game-score :initform 0
+               :accessor game-score)
+   (game-highscore :initform *game-highscore*
+                   :accessor game-highscore)
+   (game-debug-output :initform nil
+                      :accessor game-debug-output))
     (:menu-bar nil)
     (:panes
      ;; (window-title :title
      ;;               :display-string "2048-McCLIM")
-     (game-main :application
-                :width 800
-                :height 800
-                :scroll-bars nil
-                :background +color-app-background+)
      (title-label :label
                   :label "2048-McCLIM"
                   :align-x :center
                   :text-style *title-text-style*)
-     (cell-0 (make-game-cell 0 0))
-     (cell-1 (make-game-cell 0 1))
-     (cell-2 (make-game-cell 0 2))
-     (cell-3 (make-game-cell 0 3))
-     (cell-4 (make-game-cell 1 0))
-     (cell-5 (make-game-cell 1 1))
-     (cell-6 (make-game-cell 1 2))
-     (cell-7 (make-game-cell 1 3))
-     (cell-8 (make-game-cell 2 0))
-     (cell-9 (make-game-cell 2 1))
-     (cell-A (make-game-cell 2 2))
-     (cell-B (make-game-cell 2 3))
-     (cell-C (make-game-cell 3 0))
-     (cell-D (make-game-cell 3 1))
-     (cell-E (make-game-cell 3 2))
-     (cell-F (make-game-cell 3 3)))
+     (score-box :application
+                :align-x :right
+                :scroll-bars nil
+                :display-function 'draw-score-box)
+     (tag-line :label
+               :label "Join the numbers and get to the 2048 tile!")
+     (new-game-button :push-button
+                      :label "New game")
+     (timer-box :text-field
+                :editable-p nil
+                :value "00:00:00")
+     (game-main :application
+                :width 800
+                :height 800
+                :scroll-bars nil
+                :background +color-app-background+
+                :display-function 'draw-game-board)
+     (instructions-box :label
+                       :label "Use WASD keys to play.")
+     (debug-output :text-field          ;would prefer it to be a label, but whatevs.
+                   :editable-p nil
+                   :value "<DEBUG OUTPUT>"))
     (:layouts
      (default
          (clim:vertically ()
            title-label
+           score-box
+           (clim:horizontally ()
+             tag-line new-game-button)
+           timer-box
            game-main
-           (clim:tabling (:grid nil)
-             (list cell-0 cell-1 cell-2 cell-3)
-             (list cell-4 cell-5 cell-6 cell-7)
-             (list cell-8 cell-9 cell-A cell-B)
-             (list cell-C cell-D cell-E cell-F))))))
+           instructions-box
+           (setf (game-debug-output clim:*application-frame*) debug-output)))))
 
+
+;;; game logic
+;;; TODO
+
+
+;;; input
+;;; TODO Figure out one day how to use arrow keys instead.
+(define-2048-game-command (move-left :keystroke #\a)
+    ()
+  (let ((label (game-debug-output clim:*application-frame*)))
+    (setf (clim:gadget-value label) "Move left invoked!")))
+
+(define-2048-game-command (move-right :keystroke #\d)
+    ()
+  (let ((label (game-debug-output clim:*application-frame*)))
+    (setf (clim:gadget-value label) "Move right invoked!")))
+
+(define-2048-game-command (move-up :keystroke #\w)
+    ()
+  (let ((label (game-debug-output clim:*application-frame*)))
+    (setf (clim:gadget-value label) "Move up invoked!")))
+
+(define-2048-game-command (move-down :keystroke #\s)
+    ()
+  (let ((label (game-debug-output clim:*application-frame*)))
+    (setf (clim:gadget-value label) "Move down invoked!")))
+
+
+;;; drawing
+
+(defun draw-board-cell (stream board row column)
+  ;; TODO prettier game board
+  (format stream "|(~A ~A) - ~A|" row column (aref board row column)))
+
+(defmethod draw-game-board ((2048-game 2048-game) stream &key max-width max-height)
+  (declare (ignore max-width max-height))
+  (clim:formatting-table (stream)
+    (dotimes (row 4)
+      (clim:formatting-row (stream)
+        (dotimes (column 4)
+          (clim:formatting-cell (stream)
+            (draw-board-cell stream (game-board 2048-game) row column)))))))
+
+(defmethod draw-score-box ((2048-game 2048-game) stream &key max-width max-height)
+  (declare (ignore max-width max-height))
+  (clim:formatting-table (stream)
+    (clim:formatting-column (stream)
+      (clim:formatting-cell (stream :align-x :center)
+        (format stream "SCORE"))
+      (clim:formatting-cell (stream :align-x :center)
+        (format stream "~A" (game-score 2048-game))))
+    (clim:formatting-column (stream)
+      (clim:formatting-cell (stream :align-x :center)
+        (format stream "BEST"))
+      (clim:formatting-cell (stream :align-x :center)
+        (format stream "~A" (game-highscore 2048-game))))))
+
+;;; entry point
 (defun run ()
   (clim:run-frame-top-level (clim:make-application-frame '2048-game)))
